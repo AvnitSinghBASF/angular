5b20f20929d41039b66f4974c001c916fd4fa8ff
Avnit Singh <avnit.singh@basf.com>
2024-04-10 15:44:13 +0200

    nr 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {HEROES} from '../mock-heroes';
+// import {HEROES} from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+
 
 
 @Component({
@@ -18,10 +20,19 @@ export class HeroesComponent {
     id: 1,
     name: 'Windstorm'
   };
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  constructor(private heroService: HeroService) {}
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
 }
 
